29061a361781579ce49e89c77037b6e80a1a1394
shanu <shanu.futura@gmail.com>
2022-10-03 19:00:26 +0530

    package imports

---
+++ bin/socket_server.dart
@@ -0,0 +1,18 @@
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+
+void main() {
+  var handler = webSocketHandler((webSocket) {
+    webSocket.stream.listen((message) {
+      webSocket.sink.add("echo $message");
+    });
+    
+  });
+
+
+
+
+  shelf_io.serve(handler, 'localhost', 8080).then((server) {
+    print('Serving at ws://${server.address.host}:${server.port}');
+  });
+}
